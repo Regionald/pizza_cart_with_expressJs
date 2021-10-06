5b81398dc70436afea6f9d7e3fd8c531dd1aaa96
Regionald <regionaldmongwe@gmail.com>
2021-10-06 13:07:27 +0200

    tired

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 const express = require('express');
 const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
 const cart = require('./factory.js');
 const carter = cart();
 const app = express();
@@ -15,78 +14,15 @@ app.set('view engine', 'handlebars');
 
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static('public'));
 let PORT = process.env.PORT || 8081;
 
 app.get('/', (req, res) => {
-
-    var smallTotal = carter.smallValue() * 22.99;
-    var mediumTotal = carter.mediumValue() * 60.50;
-    var largeTotal = carter.largeValue() * 120.75;
-
-    res.render('index', {
-    
-        mediumQty: carter.mediumValue(),
-        smallQty: carter.smallValue(),
-        largeQty: carter.largeValue(),
-        smallTotal: smallTotal.toFixed(2),
-        mediumTotal: mediumTotal.toFixed(2),
-        largeTotal: largeTotal.toFixed(2),
-        total: carter.totalPrice().toFixed(2),
-        unhide: carter.unhide(),
-        uncart: carter.hide(),
-        message:carter.mes()
-    });
-});
-app.get('/Sadd', (req, res) => {
-    carter.qtyIncrement('small');
-    res.redirect('/');
-})
-app.get('/Ssub', (req, res) => {
-    carter.qtydecrement('small');
-    res.redirect('/');
+    res.render('index');
 })
 
-app.get('/Madd', (req, res) => {
-    carter.qtyIncrement('medium');
-    res.redirect('/');
-})
-app.get('/Msub', (req, res) => {
-    carter.qtydecrement('medium');
-    res.redirect('/');
-})
-app.get('/Ladd', (req, res) => {
-    carter.qtyIncrement('large');
-    res.redirect('/');
-})
-app.get('/Lsub', (req, res) => {
-    carter.qtydecrement('large');
-    res.redirect('/');
-})
-
-app.get('/small', (req, res) => {
-    carter.cartOpener();
-    carter.qtyIncrement('small');
-    res.redirect('/');
-})
-app.get('/medium', (req, res) => {
-    carter.cartOpener();
-    carter.qtyIncrement('medium');
-    res.redirect('/');
-})
-app.get('/large', (req, res) => {
-    carter.cartOpener();
-    carter.qtyIncrement('large');
-    res.redirect('/');
-})
-app.post('/amount', (req, res) => {
-    carter.respond(req.body.amount);
-    console.log(req.body.amount);
-    res.redirect('/');
-
-})
 
 app.listen(PORT, 'localhost', () => {
     console.log("The server is listening at port:" + PORT);
