fd64aef2128056bdce35f51bea6d265915e67409
Regionald <regionaldmongwe@gmail.com>
2021-10-18 19:45:48 +0200

    main router

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ open({
 
             app.get('/', async (req, res) => {
 
-                if (!req.session.email) {
-                    res.redirect('/login');
-                    return;
-                };
+                // if (!req.session.email) {
+                //     res.redirect('/login');
+                //     return;
+                // };
 
                 var show = 'hidden';
 
@@ -67,156 +67,156 @@ open({
                 });
             })
 
-            app.post('/small', (req, res) => {
+        //     app.post('/small', (req, res) => {
 
-                const order = usercounter.getCart(req.session.email);
+        //         const order = usercounter.getCart(req.session.email);
 
-                if (req.body.small == '+') {
-                    order.qtyIncrement('small');
-                }
-                if (req.body.small == 'Buy') {
-                    order.qtyIncrement('small');
-                }
-                if (req.body.small == '-') {
-                    order.qtydecrement('small');
-                };
+        //         if (req.body.small == '+') {
+        //             order.qtyIncrement('small');
+        //         }
+        //         if (req.body.small == 'Buy') {
+        //             order.qtyIncrement('small');
+        //         }
+        //         if (req.body.small == '-') {
+        //             order.qtydecrement('small');
+        //         };
 
-                req.session.smallValue = order.smallValue();
-                req.session.total = order.totalPrice().toFixed(2);
-                console.log(req.session.total);
-                console.log(req.session.smallValue);
-                console.log(req.sessionID);
+        //         req.session.smallValue = order.smallValue();
+        //         req.session.total = order.totalPrice().toFixed(2);
+        //         console.log(req.session.total);
+        //         console.log(req.session.smallValue);
+        //         console.log(req.sessionID);
 
-                res.redirect('/');
-            })
+        //         res.redirect('/');
+        //     })
 
-            app.post('/medium', (req, res) => {
+        //     app.post('/medium', (req, res) => {
 
-                const order = usercounter.getCart(req.session.email);
-
-
-                if (req.body.medium == '+') {
-                    order.qtyIncrement('medium');
-                }
-                if (req.body.medium == 'Buy') {
-                    order.qtyIncrement('medium');
-                }
-                if (req.body.medium == '-') {
-                    order.qtydecrement('medium');
-                };
-
-                req.session.mediumValue = order.mediumValue();
-                req.session.total = order.totalPrice().toFixed(2);
-
-                console.log(req.session.mediumValue);
-                console.log(req.sessionID);
-
-                res.redirect('/');
-            })
-
-            app.post('/large', (req, res) => {
-
-                const order = usercounter.getCart(req.session.email);
+        //         const order = usercounter.getCart(req.session.email);
 
 
-                if (req.body.large == '+') {
-                    order.qtyIncrement('large');
-                }
-                if (req.body.large == 'Buy') {
-                    order.qtyIncrement('large');
-                }
-                if (req.body.large == '-') {
-                    order.qtydecrement('large');
-                };
+        //         if (req.body.medium == '+') {
+        //             order.qtyIncrement('medium');
+        //         }
+        //         if (req.body.medium == 'Buy') {
+        //             order.qtyIncrement('medium');
+        //         }
+        //         if (req.body.medium == '-') {
+        //             order.qtydecrement('medium');
+        //         };
+
+        //         req.session.mediumValue = order.mediumValue();
+        //         req.session.total = order.totalPrice().toFixed(2);
+
+        //         console.log(req.session.mediumValue);
+        //         console.log(req.sessionID);
+
+        //         res.redirect('/');
+        //     })
+
+        //     app.post('/large', (req, res) => {
+
+        //         const order = usercounter.getCart(req.session.email);
 
 
-                req.session.total = order.totalPrice().toFixed(2);
-                req.session.largeValue = order.largeValue();
-                console.log(req.session.largeValue);
-                console.log(req.sessionID);
-                res.redirect('/');
-            })
-
-            app.get('/login', (req, res) => {
-                res.render('login');
-            })
-
-            app.post('/login', (req, res) => {
-                console.log(req.body.email);
-                if (req.body.email) {
-                    req.session.email = req.body.email;
-                    res.redirect('/');
-                }
-                else {
-                    res.redirect('/login');
-                };
+        //         if (req.body.large == '+') {
+        //             order.qtyIncrement('large');
+        //         }
+        //         if (req.body.large == 'Buy') {
+        //             order.qtyIncrement('large');
+        //         }
+        //         if (req.body.large == '-') {
+        //             order.qtydecrement('large');
+        //         };
 
 
-            })
+        //         req.session.total = order.totalPrice().toFixed(2);
+        //         req.session.largeValue = order.largeValue();
+        //         console.log(req.session.largeValue);
+        //         console.log(req.sessionID);
+        //         res.redirect('/');
+        //     })
 
-            app.get('/order', async (req, res) => {
+        //     app.get('/login', (req, res) => {
+        //         res.render('login');
+        //     })
 
-                console.log(req.session.smallValue, req.session.mediumValue, req.session.largeValue, req.session.email, req.session.total)
-                const insertData = ('INSERT INTO ORDERS (userName,smallQty,mediumQty,largeQty,total)  VALUES (?,?,?,?,?)');
-                await db.run(insertData, req.session.email, req.session.smallValue, req.session.mediumValue, req.session.largeValue, req.session.total);
-                res.redirect('/cart');
-            })
-            app.get('/cart', async (req, res) => {
+        //     app.post('/login', (req, res) => {
+        //         console.log(req.body.email);
+        //         if (req.body.email) {
+        //             req.session.email = req.body.email;
+        //             res.redirect('/');
+        //         }
+        //         else {
+        //             res.redirect('/login');
+        //         };
 
-                if (!req.session.email) {
-                    res.redirect('/login');
-                }
 
-                const orders = 'SELECT * from ORDERS where userName=? AND status=?'
-                const order = await db.all(orders, req.session.email,'payment due');
+        //     })
 
-                const paids = 'SELECT * from ORDERS where userName=? AND status=?'
-                const paid = await db.all(paids, req.session.email,'paid');
+        //     app.get('/order', async (req, res) => {
 
-                const collects = 'SELECT * from ORDERS where userName=? AND status=?'
-                const collect = await db.all(collects, req.session.email,'collected');
+        //         console.log(req.session.smallValue, req.session.mediumValue, req.session.largeValue, req.session.email, req.session.total)
+        //         const insertData = ('INSERT INTO ORDERS (userName,smallQty,mediumQty,largeQty,total)  VALUES (?,?,?,?,?)');
+        //         await db.run(insertData, req.session.email, req.session.smallValue, req.session.mediumValue, req.session.largeValue, req.session.total);
+        //         res.redirect('/cart');
+        //     })
+        //     app.get('/cart', async (req, res) => {
+
+        //         if (!req.session.email) {
+        //             res.redirect('/login');
+        //         }
+
+        //         const orders = 'SELECT * from ORDERS where userName=? AND status=?'
+        //         const order = await db.all(orders, req.session.email,'payment due');
+
+        //         const paids = 'SELECT * from ORDERS where userName=? AND status=?'
+        //         const paid = await db.all(paids, req.session.email,'paid');
+
+        //         const collects = 'SELECT * from ORDERS where userName=? AND status=?'
+        //         const collect = await db.all(collects, req.session.email,'collected');
                 
                 
-                res.render('order', { orders: order,paid:paid,collect:collect })
+        //         res.render('order', { orders: order,paid:paid,collect:collect })
 
-            })
-            app.post('/pay/:id', async (req, res) => {
+        //     })
+        //     app.post('/pay/:id', async (req, res) => {
 
                
-                req.session.amount = req.body.amount;
-                if (req.session.amount==req.session.total){
-                    const idno = req.params.id;
-                    console.log(idno);
-                    const update = 'UPDATE ORDERS set status=? where id=?'
-                    await db.all(update, 'paid', idno);
-                    res.redirect('/cart');
-                }
-                else if(req.session.amount>req.session.total){
-                    const idno = req.params.id;
-                    const update = 'UPDATE ORDERS set status=? where id=?'
-                    await db.all(update, 'paid', idno);
-                    res.redirect('/cart');
-                }
-                else{
-                    res.redirect('/cart');
-                };
+        //         req.session.amount = req.body.amount;
+        //         if (req.session.amount==req.session.total){
+        //             const idno = req.params.id;
+        //             console.log(idno);
+        //             const update = 'UPDATE ORDERS set status=? where id=?'
+        //             await db.all(update, 'paid', idno);
+        //             res.redirect('/cart');
+        //         }
+        //         else if(req.session.amount>req.session.total){
+        //             const idno = req.params.id;
+        //             const update = 'UPDATE ORDERS set status=? where id=?'
+        //             await db.all(update, 'paid', idno);
+        //             res.redirect('/cart');
+        //         }
+        //         else{
+        //             res.redirect('/cart');
+        //         };
      
-            })
-            app.post('/collect/:id', async (req, res) => {
+        //     })
+        //     app.post('/collect/:id', async (req, res) => {
 
-                    const idno = req.params.id;
-                    const update = 'UPDATE ORDERS set status=? where id=?'
-                    await db.all(update, 'collected', idno);
-                    res.redirect('/cart');
-            })
-            app.post('/remove/:id', async (req, res) => {
+        //             const idno = req.params.id;
+        //             const update = 'UPDATE ORDERS set status=? where id=?'
+        //             await db.all(update, 'collected', idno);
+        //             res.redirect('/cart');
+        //     })
+        //     app.post('/remove/:id', async (req, res) => {
 
-                const idno = req.params.id;
-                const delet = 'DELETE from ORDERS where id=?'
-                const deleted = await db.run(delet,idno);
-                res.redirect('/cart');
+        //         const idno = req.params.id;
+        //         const delet = 'DELETE from ORDERS where id=?'
+        //         const deleted = await db.run(delet,idno);
+        //         res.redirect('/cart');
                 
-           })
+        //    })
 
             app.listen(PORT, () => {
                 console.log("The server is listening at port:" + PORT);
